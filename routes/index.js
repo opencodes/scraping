2866888cf19a4d78501faf9bd95e05ca0084a214
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2013-10-29 09:52:18 +0530

    google scrap

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,31 @@
 /*
  * GET home page.
  */
-var nodeio = require('node.io');
+var request = require('request'),
+	jsdom = require('jsdom');
 
 exports.index = function(req, res){
 	nodeio.scrape(function() {
-	    this.getHtml('http://www.rkjha.com', function(err, $) {
-	        var stories = [];
-	        $('a').each(function(title) {
-	            stories.push($(this).attr('href'));
-	        });
-	        console.log(err);console.log($);
-	        res.render('index', { title: 'Express',data : stories });
-	    });
+		
+
+		request({ uri:'http://www.google.com' }, function (error, response, body) {
+		  if (error && response.statusCode !== 200) {
+		    console.log('Error when contacting google.com');
+		  }
+		  
+		  jsdom.env({
+		    html: body,
+		    scripts: [
+		      'http://code.jquery.com/jquery-1.5.min.js'
+		    ]
+		  }, function (err, window) {
+		    var $ = window.jQuery;
+		
+		    // jQuery is now loaded on the jsdom window created from 'agent.body'
+		    console.log($('body').html());
+		  });
+		});
+		res.render('index', { title: 'Express',data : stories });
 	});
 	
 };
